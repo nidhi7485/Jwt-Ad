c3f56dcafbe4d8f3d1bc27e2fc626238c2d4e7a4
nidhi <nidhi7485@gmail.com>
2022-10-17 14:47:28 +0530

    JWT generated

--- controllers/main.js
+++ controllers/main.js
@@ -1,5 +1,17 @@
+const CustomApiError = require('../errors/custom-error')
+const jwt = require('jsonwebtoken')
 const login = async (req, res) => {
-  res.send('here is dashboard || register || login ')
+  const { username, password } = req.body
+  console.log(username, password)
+  if (!username || !password) {
+    throw new CustomApiError('please provide username and password', 400)
+  }
+
+  const id = new Date().getDate()
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  })
+  res.status(200).json({ msg: 'user created', token })
 }
 
 const dashboard = async (req, res) => {
